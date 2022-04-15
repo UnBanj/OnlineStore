6bd65de42f6385b36186d130483ea67a7378ad5f
Una <ubanjanin@gmail.com>
2022-04-15 16:57:39 +0200

    Add main menu items property for dynamic creation

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
 
-export class MainMenu extends React.Component {
+export class MainMenuItem {
+    text: string = '';
+    link: string= '#';
+
+    constructor(text: string, link: string){
+        this.text= text;
+        this.link = link;
+    }
+}
+
+interface MainMenuProperties {
+    items: MainMenuItem[];
+}
+
+export class MainMenu extends React.Component<MainMenuProperties> {
     render(){
         return (
            <Nav variant="tabs">
-             <Nav.Link href="/">Home</Nav.Link>
-             <Nav.Link href="/contact">Contact</Nav.Link>
-             <Nav.Link href="/login">Log in</Nav.Link>
-           </Nav>
+             { this.props.items.map(this.makeNavLink) }
+            </Nav>
         );   
+    }
+    //uzima 1 item i vraca reprezentaciju tog itema u obliku html koda
+    private makeNavLink(item: MainMenuItem){
+        return (
+            <Nav.Link href= {item.link}>
+                {item.text}
+            </Nav.Link>
+        );
     }
 }
