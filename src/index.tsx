11fb4a483e45a6422b9b42c2267fb79d1ed90d01
Una <ubanjanin@gmail.com>
2022-05-12 16:37:17 +0200

    Load top level categories on the homepage

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ import { HashRouter, Route, Switch} from 'react-router-dom';
 import ContactPage from './components/ContactPage/ContactPage';
 import UserLoginPage from './components/UserLoginPage/UserLoginPage';
 import CategoryPage from './components/CategoryPage/CategoryPage';
+import { UserRegistrationPage } from './components/UserRegistrationPage/UserRegistrationPage';
 
 
 
@@ -20,12 +21,9 @@ const menuItems = [
   new MainMenuItem("Home","/"),
   new MainMenuItem("Contact","/contact/"),
   new MainMenuItem("Log in","/user/login/"),
+  new MainMenuItem("Register","/user/register/"),
 
-  new MainMenuItem("Cat 1","/category/1/"),
-  new MainMenuItem("Cat 7","/category/7/"),
-  new MainMenuItem("Cat 21","/category/21/"),
-  
-];
+ ];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -39,6 +37,7 @@ root.render(
          <Route exact path="/" component={ HomePage}/>
          <Route  path="/contact" component={ ContactPage} />
          <Route  path="/user/login" component={ UserLoginPage} />
+         <Route  path="/user/register" component={ UserRegistrationPage} />
          <Route path="/category/:cId" component={ CategoryPage } />
         </Switch> 
       
